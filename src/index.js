8dbff9d6487b6624af116adb0a00d5cf7508b2ea
flavio <flavio.maccarrone.98@gmail.com>
2019-07-15 18:07:08 +0200

    Esplorazione react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function showContry(props){
+    alert (props.name);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class View extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="title">
+                    <p> CONTINENTI TERRESTRI </p>
+                </div>
+                <div className="list">
+                 <Continent
+                 />
+                </div>
+                <br/><br/>
+                <Footer
+                />
+            </div>
+        );
+    }
+}
+
+class Continent extends React.Component{
+    state = {
+        continents: []
+    }
+
+    componentDidMount(){
+
+        axios.get(`http://localhost:8080/continent`)
+        .then(res => {
+            const continents = res.data;
+            this.setState({ continents });
+        })
+    }
+
+    handleClick(continent){
+        
+        <showContry name = {continent}  />;
+        return "prova";
+    }
+ 
+    render(){
+        return (
+               this.state.continents.map(continents => <a key={continents} onClick={()=>this.handleClick(continents)}> {continents}<br/></a>)
+          )
+    }
+}
+
+class Country extends React.Component{
+
+    myClick(){
+        alert(this.props.continent);
+    }
+
+    render(){
+        return "";
+    }
+}
+
+class Footer extends React.Component{
+    render(){
+        return (
+            <div className="footer">
+		        <button type="button" id="backButton" value="">TORNA INDIETRO</button>
+		        <input type="hidden" id="infoBackPage" value="" />
+		        <input type="hidden" id="infoCurrentPage" value="" />
+            </div>    
+        )
+    }
+}
+
+/*===============================*/
+ReactDOM.render(
+    <View />,
+    document.getElementById('root')
+  );
+
